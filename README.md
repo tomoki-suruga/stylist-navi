900a144a20a6f3492380674f9f8845f03b7e1805
yuki fujita <yfujita@yukinoMacBook-Pro.local>
2021-11-14 00:35:08 +0900

    initial implemant

--- README.md
+++ README.md
@@ -5,3 +5,29 @@ ranking site for stylist
 ##### [DB](./db/README.md)
 
 ##### [WEB APP](./webapp/README.md)
+
+##### 初期化
+
+- 環境変数をローカルに設定(開発は`development`, 本番は`production`が入る想定)
+
+```
+export NODE_ENV=local
+```
+
+- テーブル作成
+
+```
+# テーブル定義のテンプレート作成(Usersテーブルの場合)
+npx sequelize model:generate --name User --attributes firstName:string,lastName:string,email:string
+# テーブル作成の実行
+npx sequelize db:migrate
+```
+
+- シードデータ投入
+
+```
+# シードデータのテンプレート作成(もうある場合は省略可)
+npx sequelize seed:generate --name seed-user
+# insertの実行
+npx sequelize db:seed:all
+```
--- webapp/node/app/src/app.ts
+++ webapp/node/app/src/app.ts
@@ -1,28 +1,115 @@
-import express = require("express");
-const app: express.Express = express();
-const port: string = process.env.PORT || "3000";
+import express = require("express")
+import {
+  DB_CONNECTION_ERROR,
+  FAILED_REGISTER,
+  NO_DATA,
+  CREATE_USER_SUCCESSED,
+  UPDATE_USER_SUCCESSED,
+} from "./const/messages"
+const db = require("./models/")
+const app: express.Express = express()
+const port: string = process.env.PORT || "3000"
+const getRouter = express.Router()
+const postRouter = express.Router()
 
-type Users = {
-  id: string;
-  name: string;
-};
+getRouter.get("/api/users", async (_, res: express.Response) => {
+  await db.User.findAll()
+    .then((users: any) => {
+      if (!users) {
+        console.log(NO_DATA)
+      } else {
+        res.json(users)
+      }
+    })
+    .catch((e: Error) => {
+      console.log(e)
+      console.log(DB_CONNECTION_ERROR)
+      res.status(500).send(DB_CONNECTION_ERROR)
+    })
+})
 
-const users = [
-  { id: "01", name: "hoge1" },
-  { id: "02", name: "fuga2" },
-  { id: "03", name: "piyo3" },
-];
-
-class Main {
-  constructor() {
-    app.get("/", (req: express.Request, res: express.Response) => {
-      res.send("Simple REST API");
-    });
-    app.get("/api/users", (req: express.Request, res: express.Response) => {
-      res.send(users);
-    });
-    app.listen(port, () => console.log(`Listening on port ${port}...`));
+getRouter.get(
+  "/api/users/:id",
+  async (req: express.Request, res: express.Response) => {
+    await db.User.findByPk(req.params.id)
+      .then((user: any) => {
+        if (!user) {
+          console.log(NO_DATA)
+        } else {
+          res.json(user)
+        }
+      })
+      .catch((e: Error) => {
+        console.log(e)
+        console.log(DB_CONNECTION_ERROR)
+        res.status(500).send(DB_CONNECTION_ERROR)
+      })
   }
-}
+)
 
-const main = new Main();
+postRouter.use(express.json())
+postRouter.post(
+  "/api/users/create",
+  async (req: express.Request, res: express.Response) => {
+    const t = await db.sequelize.transaction()
+    await db.sequelize.transaction({}, async (transaction: any) => {
+      await db.User.create(
+        {
+          name: req.body.user.name,
+          email: req.body.user.email,
+        },
+        { transaction: t }
+      )
+        .then((result: any) => {
+          if (!result) {
+            console.log(FAILED_REGISTER)
+          } else {
+            console.log(CREATE_USER_SUCCESSED)
+            res.status(200).send(CREATE_USER_SUCCESSED)
+          }
+        })
+        .catch((e: Error) => {
+          console.log(e)
+          console.log(DB_CONNECTION_ERROR)
+          res.status(500).send(DB_CONNECTION_ERROR)
+        })
+    })
+  }
+)
+
+postRouter.post(
+  "/api/users/update",
+  async (req: express.Request, res: express.Response) => {
+    db.sequelize.transaction({}, async (transaction: any) => {
+      await db.User.update(
+        {
+          name: req.body.user.name,
+          email: req.body.user.email,
+        },
+        {
+          transaction,
+          where: {
+            id: req.body.user.id,
+          },
+        }
+      )
+        .then((result: number) => {
+          if (!result) {
+            console.log(FAILED_REGISTER)
+          } else {
+            console.log(UPDATE_USER_SUCCESSED)
+            res.status(200).send(UPDATE_USER_SUCCESSED)
+          }
+        })
+        .catch((e: Error) => {
+          console.log(e)
+          console.log(DB_CONNECTION_ERROR)
+          res.status(500).send(DB_CONNECTION_ERROR)
+        })
+    })
+  }
+)
+
+app.use(getRouter)
+app.use(postRouter)
+app.listen(port, () => console.log(`Listening on port ${port}...`))
